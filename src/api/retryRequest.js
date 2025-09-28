d591138dd784444e67df4e5398a50e8870f9784e
Vijayant Joshi <jobs.vijayantjoshi@gmail.com>
2025-09-28 12:22:31 +0530

    Modification in login and api

--- src/api/retryRequest.js
+++ src/api/retryRequest.js
@@ -13,16 +13,14 @@ const retryRequest = async (fn, retries = 3, delay = 1000) => {
                 if (i < retries - 1) {
                     toastId = toast.loading(`Retrying... (${i + 1}/${retries})`, {
                         id: "retry-toast",
-                        duration: delay,
+                        duration: delay
                     });
                     await new Promise(res => setTimeout(res, delay));
                 } else {
                     toast.dismiss("retry-toast");
                     throw new Error("Network error after multiple retries. Please try again later.");
-                }
-            } else {
-                throw error;
-            };
+                };
+            } else throw error;
         };
     };
 };
